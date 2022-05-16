09ab5b014c9f987ed69335cc177925cbd7eeb85e
WARRIORS <56763972+ccadmin1@users.noreply.github.com>
2022-05-16 11:07:16 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -12,14 +12,14 @@ logging.basicConfig(
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
+    API_ID = "9244035"
+    API_HASH = "66c62a85cfbf593a991ea680223a0549"
+    BOT_TOKEN = "5062336126:AAE_8j8jCft2ZC17pXSiET7_dU_myDvMLp8"
+    DOWNLOAD_DIR = "./downloads"
     LOGGER = logging
-    OWNER_ID = int(os.environ.get("OWNER_ID", "5265780945"))
+    OWNER_ID = "5088475411"
     PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
     PRO_USERS.append(OWNER_ID)
-    MONGODB_URI = os.environ.get("MONGODB_URI")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001790184567"))
-    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
+    MONGODB_URI = "mongodb+srv://Aj:Aj@cluster0.pvz0y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    LOG_CHANNEL = "-1001673475040"
+    BROADCAST_AS_COPY = "False"
